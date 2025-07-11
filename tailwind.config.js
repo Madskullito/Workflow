f1bdf295a39eb5898a3bb94e93ce484236d096a1
Madskullito <contacto@madskull.info>
2025-07-11 17:29:20 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-  theme: { extend: {} },
-  plugins: []
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E3A8A',    // azul oscuro
+        secondary: '#059669',  // verde
+      },
+    },
+  },
+  plugins: [],
 };
